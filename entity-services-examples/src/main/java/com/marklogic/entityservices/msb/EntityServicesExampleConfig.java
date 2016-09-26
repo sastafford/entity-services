83fbc7524064f8462e66797a2d4e63604dcbadeb
Scott Stafford <sstafford@marklogic.com>
2016-09-26 15:10:45 -0400

    Add harmonizer step

--- entity-services-examples/src/main/java/com/marklogic/entityservices/msb/EntityServicesExampleConfig.java
+++ entity-services-examples/src/main/java/com/marklogic/entityservices/msb/EntityServicesExampleConfig.java
@@ -4,11 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.document.DocumentWriteOperation;
+import com.marklogic.client.document.ServerTransform;
 import com.marklogic.client.helper.DatabaseClientProvider;
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.Format;
 import com.marklogic.client.io.JacksonHandle;
 import com.marklogic.client.io.MarkLogicWriteHandle;
+import com.marklogic.client.query.StructuredQueryBuilder;
+import com.marklogic.client.query.StructuredQueryDefinition;
+import com.marklogic.datamovement.ApplyTransformListener;
+import com.marklogic.datamovement.DataMovementManager;
+import com.marklogic.datamovement.JobTicket;
+import com.marklogic.datamovement.QueryHostBatcher;
 import com.marklogic.spring.batch.item.MarkLogicItemWriter;
 import com.marklogic.spring.batch.item.processor.ResourceToDocumentWriteOperationItemProcessor;
 import com.marklogic.spring.batch.item.reader.EnhancedResourcesItemReader;
@@ -16,16 +23,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.item.*;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EnvironmentAware;
@@ -37,6 +49,7 @@ import org.springframework.validation.BindException;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 @EnableBatchProcessing
 public class EntityServicesExampleConfig implements EnvironmentAware {
@@ -54,9 +67,14 @@ public class EntityServicesExampleConfig implements EnvironmentAware {
     public Job job(
         JobBuilderFactory jobBuilderFactory,
         @Qualifier("importJsonData") Step importJsonData,
-        @Qualifier("importCsvData") Step importCsvData
+        @Qualifier("importCsvData") Step importCsvData,
+        @Qualifier("harmonizerStep") Step harmonizer
     ) {
-        return jobBuilderFactory.get(JOB_NAME).start(importJsonData).next(importCsvData).build();
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(importJsonData)
+                .next(importCsvData)
+                .next(harmonizer)
+                .build();
     }
     
     @Bean
@@ -78,7 +96,7 @@ public class EntityServicesExampleConfig implements EnvironmentAware {
         processor.setMetadataHandle(metadata);
         ItemWriter<DocumentWriteOperation> writer = new MarkLogicItemWriter(databaseClient);
         
-        return stepBuilderFactory.get("step1")
+        return stepBuilderFactory.get("importJsonData")
                 .<Resource, DocumentWriteOperation>chunk(10)
                 .reader(new EnhancedResourcesItemReader(inputFilePath, inputFilePattern))
                 .processor(processor)
@@ -115,12 +133,47 @@ public class EntityServicesExampleConfig implements EnvironmentAware {
         
         ItemWriter<DocumentWriteOperation> itemWriter = new MarkLogicItemWriter(databaseClientProvider.getDatabaseClient());
         
-        return stepBuilderFactory.get("step1")
+        return stepBuilderFactory.get("importCsvData")
                 .<Run, DocumentWriteOperation>chunk(10)
                 .reader(itemReader)
                 .processor(itemProcessor)
                 .writer(itemWriter)
                 .build();
+    }
+    
+    @Bean
+    @JobScope
+    public Step harmonizerStep(
+            StepBuilderFactory stepBuilderFactory,
+            DatabaseClientProvider databaseClientProvider) {
+        
+        DataMovementManager moveMgr = DataMovementManager.newInstance().withClient(databaseClientProvider.getDatabaseClient());
+        StructuredQueryBuilder qb = new StructuredQueryBuilder();
+        StructuredQueryDefinition qdef = qb.collection("raw");
+        ServerTransform ingester = new ServerTransform("ingester");
+        ApplyTransformListener listener = new ApplyTransformListener().withTransform(ingester)
+                .withApplyResult(ApplyTransformListener.ApplyResult.IGNORE).onSuccess((dbClient, inPlaceBatch) -> {
+                    logger.debug("Batch transform SUCCESS");
+                }).onBatchFailure((dbClient, inPlaceBatch, throwable) -> {
+                    System.err.println(throwable.getMessage());
+                    System.err.print(String.join("\n", inPlaceBatch.getItems()) + "\n");
+                });
+        
+        Tasklet tasklet = new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                QueryHostBatcher queryHostBatcher = moveMgr.newQueryHostBatcher(qdef).withBatchSize(100)
+                        .withThreadCount(5).onUrisReady(listener).onQueryFailure((client3, exception) -> {
+                            logger.error("Query error");
+                        });
+    
+                JobTicket ticket = moveMgr.startJob(queryHostBatcher);
+                queryHostBatcher.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+                moveMgr.stopJob(ticket);
+                return RepeatStatus.FINISHED;
+            }
+        };
+        return stepBuilderFactory.get("harmonizer").tasklet(tasklet).build();
     }
     
     
